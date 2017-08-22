40131ee33207f1cc2d3a28be7815f2a653983ff0
Mark Dawson <mark.dawson@swansea.ac.uk>
2017-08-22 21:20:51 +0100

    delete QPushButtonMinSize

--- QPushButtonMinSize.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-
-
-class QPushButtonMinSize(QtWidgets.QPushButton):
-    def __init__(self, text, parent=None):
-        super(QPushButtonMinSize, self).__init__(parent)
-        self.setText(text)
-        width = self.fontMetrics().boundingRect(text).width() + 30
-        self.setMaximumWidth(width)
